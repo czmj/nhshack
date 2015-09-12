c8ddca28b471d93cd0fe34a1a675140996f7374f
czmj <clara@jordan-maynard.org>
2015-09-12 12:03:24 +0100

    basic UI with filter list

--- nhshackapp/www/js/services.js
+++ nhshackapp/www/js/services.js
@@ -1,50 +1,66 @@
-angular.module('starter.services', [])
+(function () {
+    'use strict'
+    angular
+        .module('starter.services', [])
+        .factory('dataSvc', dataSvc);
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+    function dataSvc(){
+        var exports = {
+            getDrugs: getDrugs,
+            getDrug: getDrug
+        };
+        
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
+        return exports;
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+        ////////////////
+
+        function getDrugs() {
+            var data = [
+                { 
+                    name: 'A drug', 
+                    description: 'sflkjs fd jslkfj sdlkfj sdklfj sdlkjf kslj f',
+                    id: 1 
+                },
+                { 
+                    name: 'Another drug', 
+                    description: 'sflkjs fd jslkfj sdlkfj sdklfj sdlkjf kslj f',
+                    id: 2 
+                },
+                { 
+                    name: 'B drug', 
+                    description: 'sflkjs fd jslkfj sdlkfj sdklfj sdlkjf kslj f',
+                    id: 3 
+                },
+                { 
+                    name: 'C drug',
+                    description: 'sflkjs fd jslkfj sdlkfj sdklfj sdlkjf kslj f',
+                    id: 4 
+                },
+                { 
+                    name: 'D drug', 
+                    description: 'sflkjs fd jslkfj sdlkfj sdklfj sdlkjf kslj f',
+                    id: 5 
+                },
+                {
+                    name: 'E drug', 
+                    description: 'sflkjs fd jslkfj sdlkfj sdklfj sdlkjf kslj f',
+                    id: 6 
+                }
+              ];
+            
+            return data;                
         }
-      }
-      return null;
+        
+        function getDrug(id){
+            var i,
+                drugs = getDrugs();
+                        
+            for (i=0; i<drugs.length; i++){
+                if(drugs[i].id.toString() === id){
+                    return drugs[i];
+                }
+            }
+        }
     }
-  };
-});
+})();
